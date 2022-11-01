5aa5275c885e28b19627a4aa1708262889f6591e
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-11-01 11:12:23 +0100

    Add project files.

---
+++ Classes/DailyWeather.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyWeatherData.Classes
+{
+    internal class DailyWeather
+    {
+        public string DisplayData { get; set; }
+        public DateTime Date { get; set; }
+        public bool Sunny { get; set; }
+        public double Windspeed { get; set; }
+        public double Degrees { get; set; }
+
+        public DailyWeather()
+        {
+        }
+
+        public DailyWeather(bool sunny, double windspeed, double degrees)
+        {
+            Sunny = sunny;
+            Windspeed = windspeed;
+            Degrees = degrees;
+            Date = DateTime.Now;
+
+            if (sunny)
+            {
+                DisplayData = $"Dag: {Date.ToString("yy/MM/dd")}\nSoligt Vind: {windspeed}m/s  Grader: {degrees} C";
+            }
+            else
+            {
+                DisplayData = $"Dag: {Date.ToString("yy/MM/dd")}\nInte Soligt Vind: {windspeed}m/s  Grader: {degrees} C";
+            }
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using DailyWeatherData.Classes;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace DailyWeatherData
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<DailyWeather> dailyWeathers = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addWeatherDatabtn_Click(object sender, RoutedEventArgs e)
+        {
+            bool sunnyOrNot = false, degreesParsed = false, windParsed = false;
+            double windSpeed = 0, degrees = 0;
+            if ((bool)sunnyRadiobtn.IsChecked)
+            {
+                sunnyOrNot = true;
+            }
+            else if ((bool)notSunnyRadiobtn.IsChecked)
+            {
+                sunnyOrNot = false;
+            }
+            else
+            {
+                MessageBox.Show("Du måste välja soligt eller inte", "Soligt?");
+            }
+
+            if (degreesTextBox.Text != null)
+            {
+                degreesParsed = double.TryParse(degreesTextBox.Text, out degrees);
+                if (!degreesParsed)
+                {
+                    MessageBox.Show("Du har inte angett ett giltigt gradantal\nEndast siffror och kommatecken", "Fel gradantal");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Du måste ange grader", "Ange grader");
+            }
+
+
+            if (windspeedTextBox.Text != null)
+            {
+                windParsed = double.TryParse(windspeedTextBox.Text, out windSpeed);
+            }
+            else
+            {
+                MessageBox.Show("Du måste ange temperatur", "Ange temp");
+            }
+            if (windParsed && degreesParsed && ((bool)sunnyRadiobtn.IsChecked || (bool)notSunnyRadiobtn.IsChecked))
+            {
+                DailyWeather weather = new DailyWeather(sunnyOrNot, windSpeed, degrees);
+                dailyWeathers.Add(weather);
+                MessageBox.Show("Dagen tillagd!");
+                ClearContent();
+                DisplayContent();
+            }
+        }
+
+        private void ClearContent()
+        {
+            windspeedTextBox.Clear();
+            degreesTextBox.Clear();
+            sunnyRadiobtn.IsChecked = false;
+            notSunnyRadiobtn.IsChecked = false;
+        }
+
+        public void DisplayContent()
+        {
+            weatherList.ItemsSource = null;
+            weatherList.ItemsSource = dailyWeathers;
+        }
+    }
+}
